54ea4992c40f03c342ffadacd2ed8da9c736c6ae
KamyabTabani <k.tabani82@gmail.com>
2021-01-21 01:01:10 +0330

    Tabani 1

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -1,13 +1,6 @@
 public class MAIN {
-    public static void main(String[] args) {
-
-        //new CustomerCreator();
-        //new EmployeeCreator();
-        //new ProductCreator();
-        //new ManagerCreator();
-
+    public static void main(String[] args) 
+    {
         new MainFrame();
-
-        //new ManagerFrame(new Manager("J.", "Tanha", "JTanha", "jtanha", "09142075023"));
     }
 }
