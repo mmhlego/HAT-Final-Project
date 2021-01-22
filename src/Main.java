51426725fb51986d5e5bd9cdad558b4c6d361af5
KamyabTabani <k.tabani82@gmail.com>
2021-01-22 14:49:47 +0330

    Final Bug

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-import Employee.*;
-import Manager.*;
-
 public class Main {
-    public static void main(String[] args) 
-    {
-        //new MainFrame();
-        new ManagerFrame(new Manager("J.", "Tanha", "JTanha", "jtanha", "09142075023"));
+    public static void main(String[] args) {
+        new MainFrame();
     }
 }
