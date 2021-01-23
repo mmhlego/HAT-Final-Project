f1ed6c264e50747de68fc3c657eacce5a0f99142
mmhlego <mmhlego@gmail.com>
2021-01-24 01:44:40 +0330

    merged Tabani and mmhlego

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-import Login.*;
+import Customer.*;
 
-public class Main {
-        public static void main(String[] args) {
-                new MainFrame();
-        }
+public class MAIN {
+    public static void main(String[] args) 
+    {
+        //new MainFrame();
+        new CustomerFrame(new Customer("Kamyab" , "Tabani" , "K.T" , "Kamyab.T" , "09140000000" , "Zafarniye" , 100));
+    }
 }
