c82a69d5280a34fa7a0346948a4e5adb521b6bc7
pouyaafraz123 <pouyaafraz123@gmail.com>
2021-01-25 01:46:57 +0330

    Afraz patch10 Captcha

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+import General.Captcha;
+import General.User;
 import Login.*;
 import Manager.*;
 
 public class Main {
     public static void main(String[] args) {
-        new ManagerFrame(new Manager("", "", "", "", ""));
+        //new ManagerFrame(new Manager("", "", "", "", ""));
+    	//new MainFrame();
+    	JFrame f = new JFrame();
+    	Captcha c = new Captcha(new User(),f);
+    	//c
+    	f.getContentPane().setBackground(Color.yellow);
+    	//c.setBounds(0, 0, 136,34);
+    	f.add(c);
+    	f.setVisible(true);
+    	c.setBounds(50,50,136,30);
+    	f.setLayout(null);
     }
 }
