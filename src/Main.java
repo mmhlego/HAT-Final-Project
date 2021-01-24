cfd2adf96093eda8fabaf28d8c2cc35ca8b32ba4
KamyabTabani <k.tabani82@gmail.com>
2021-01-24 18:51:14 +0330

    Dargahe Pardakht

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-import Manager.*;
+import Customer.*;
+import Login.*;
 
 public class Main {
     public static void main(String[] args) {
         //new MainFrame();
-        new ManagerFrame(new Manager("", "", "", "", ""));
+        //new ManagerFrame(new Manager("", "", "", "", ""));
+        new CustomerChargeBalance();
     }
 }
