573eb7ee0512fc040c8ddb598662c5445b04afde
mmhlego <mmhlego@gmail.com>
2021-01-30 11:25:04 +0330

    added invoice creator 100%

---
+++ Backup/RandomInvoice.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class RandomInvoice {
+    public static void RandomInvoice() {
+        try {
+            ObjectInputStream re = new ObjectInputStream(
+                    new FileInputStream(System.getProperty("user.dir") + "\\data\\Customers.dat"));
+            Customer[] all = (Customer[]) re.readObject();
+            re.close();
+
+            new InvoiceCreator(all[0].order, 975454541346l, 293451571756L);
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import Customer.*;
 import Employee.*;
 import General.*;
@@ -11,21 +10,6 @@ public class Main {
 		new EmployeeCreator();
 		new ProductCreator();
 		new CustomerCreator();
-		//new MainFrame();
-		invoice();
-	}
-
-	public static void invoice() {
-		try {
-			ObjectInputStream re = new ObjectInputStream(
-					new FileInputStream(System.getProperty("user.dir") + "\\data\\Customers.dat"));
-			Customer[] all = (Customer[]) re.readObject();
-			re.close();
-
-			new InvoiceCreator(all[0].order, 973465, 2931756);
-
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
+		new MainFrame();
 	}
 }
