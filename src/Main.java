9b87746a679ecdd5acad58ec9682112f4450b5ff
mmhlego <mmhlego@gmail.com>
2021-01-31 21:12:34 +0330

    Final merge - V1 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import Login.*;
+/*import Customer.CustomerCreator;
+import Employee.EmployeeCreator;
+import General.ProductCreator;
+import Manager.ManagerCreator;*/
 
 public class Main {
 	public static void main(String[] args) {
+		/*new ProductCreator();
+		new CustomerCreator();
+		new EmployeeCreator();
+		new ManagerCreator();*/
 		new MainFrame();
 	}
 }
